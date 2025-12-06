74b899662945e4fc8579bdef1b64230f0ef9f60a
francolirosario <francolirosario@gmail.com>
2025-12-06 17:04:55 -0400

    Agregando archivos de terraform, helm y ansible

---
+++ terraform/main.tf
@@ -0,0 +1,15 @@
+resource "helm_release" "i2btech-reto-app" {
+  name      = "i2btech-reto-app"
+  chart     = "../charts/i2btech-reto-app"
+  namespace = "i2btech-reto-app-ns"
+
+  set {
+    name  = "image.repository"
+    value = var.image_repository
+  }
+
+  set {
+    name  = "image.tag"
+    value = var.image_tag
+  }
+}
---
+++ terraform/providers.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.11"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.32"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
